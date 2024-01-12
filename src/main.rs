9c2156705dc0e8b4114a6e90f41d3135704aee33
luke-e-gibson <luke.e.gibson@outlook.com>
2024-01-12 21:49:36 +1100

    tets

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+extern crate ev3dev_lang_rust;
+use ev3dev_lang_rust::Ev3Result;
+
+
+fn main() -> Ev3Result<()> {
+
+    // Get current rgb color tuple.
+    println!("Hello World");
+
+    Ok(())
+}
